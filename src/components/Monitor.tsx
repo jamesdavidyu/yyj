02714050e1d97ce3912c600a03354fe44f409905
youngjune <youngjune@planorban.com>
2024-08-22 20:23:36 -0400

    got frontend set up

---
+++ src/components/Monitor.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Box, Center, Text } from "@mantine/core";
+
+export const Monitor = () => {
+    return (
+        <Box>
+            <Text>Monitor</Text>
+        </Box>
+    )
+}
