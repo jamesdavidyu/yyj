5765b0683d60795189c1ed351fbd4c2608f5a148
You Young June <youngjune@planorban.com>
2024-08-24 00:40:23 -0400

    added light effect, need to fill in gap though

--- src/components/Lamp.tsx
+++ src/components/Lamp.tsx
@@ -1,6 +1,15 @@
+"use client";
+
 import { Box, Button, Center, Space } from "@mantine/core";
+import { useState } from "react";
 
 export const Lamp = () => {
+  const [light, setLight] = useState(true);
+
+  const lightSwitch = () => {
+    setLight((prev) => !prev);
+  };
+
   return (
     <Box>
       <Box style={{ height: "100vh" }}>
@@ -12,13 +21,27 @@ export const Lamp = () => {
               backgroundColor: "#333",
               borderRadius: "50px 50px 0px 0px",
             }}
+            onClick={lightSwitch}
           />
         </Center>
-        <Center style={{ height: "75%" }}>
-          <Box
-            style={{ width: "10%", height: "100%", backgroundColor: "#333" }}
-          />
-        </Center>
+        {light ? (
+          <Center style={{ height: "75%" }}>
+            <Box
+              style={{ width: "10%", height: "100%", backgroundColor: "#333" }}
+            />
+          </Center>
+        ) : (
+          <Center style={{ height: "75%" }}>
+            <Box
+              style={{
+                width: "100%",
+                height: "100%",
+                background: "linear-gradient(to bottom, #f0c27b, #000)",
+                clipPath: "polygon(13% 0, 87% 0%, 100% 100%, 0 100%)",
+              }}
+            />
+          </Center>
+        )}
       </Box>
     </Box>
   );
